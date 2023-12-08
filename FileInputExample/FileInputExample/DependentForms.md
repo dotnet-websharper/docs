30b38232835ba02434ca391e29fb02b48ce884ef
Gyalus Gergő <gyalus.gergo.98@gmail.com>
2023-12-08 14:28:33 +0100

    in-progress commit

--- FileInputExample/FileInputExample/DependentForms.md
+++ FileInputExample/FileInputExample/DependentForms.md
@@ -1,7 +1,7 @@
 # How to make form sequences?
 ## Method A: Dependent forms
-
-A simple username-password form with a "confirm password" field, which is only visible if we already have a valid password input, using `WebSharper.Forms`:
+With this method, you can create forms with conditionally rendered fields, such as a "hidden" confirm password field, or maybe an e-mail field depending on a selected checkbox.
+This example will show you how to create a simple username-password form with a "confirm password" field, which is only visible if we already have a valid password input, using `WebSharper.Forms`:
 ```fsharp
 open WebSharper.Forms
 type RegData = {
@@ -22,7 +22,7 @@ let ExampleForm() =
         return! Form.Yield "" // the field that we'll bind to our "password" Var
                 |> Validation.Is ((=) currentPw) "The two passwords must be an exact match!"
     }
-    |> Form.WithSubmit // otherwise validation checks will run on every input change
+    |> Form.WithSubmit // validation checks before this will run on every input change, validation checks/Form.Run after this will only run on submit.
 ```
 
 With this, you get an abstract form, for which you'll also have to define a render function using `Form.Render`, such as:
@@ -55,6 +55,62 @@ let renderExampleForm() =
 ```
 In this case, the `renderExampleForm` returns a regular doc, so you can embed it anywhere you want on your site.
 
+You can also nest these Dependent forms and split the render with any conditional checks, such as:
+```fsharp
+type FormStep =
+| Step1
+| Step2 of Step1Data
+let currStep = Var.Create Step1
+let render formWithDependent =
+    formWithDependent
+    |> Form.Render (fun formParam1 formParam2 dependent ->
+        currStep.View 
+        |> Doc.BindView (
+            function
+            | Step1 -> dependent.RenderPrimary(fun a -> ...)
+            | Step2 stepdata -> dependent.RenderDependent(fun a -> ...)
+        )
+    )
+
+```
+Not the best/most realistic use case, but if you want to do that, you can. Method B will show a way without dependents
+
 Notes:
 - Multiple `let!` bindings result multiple vars in the returned `Form.Dependent` object.
 - If a validation fails on a `let!` binding, the returned Form won't be rendered by the `formName.RenderDependent` function.
+
+## Method B: Using a Router/HashRouter
+In this example, we'll use a simple HashRouter with GET requests to implement a sign-up -> shopping cart flow, omitting the login page for the sake of simplicity.
+We can reuse the example form from Method A here with only some minor changes to our renderExampleForm function:
+```fsharp
+let renderExampleForm onSubmit =
+    ExampleForm()
+    |> Form.Run (fun (result:RegData) -> onSubmit result.UserName) // to not leak the password, we'll only call the onSubmit function with the received username
+    |> Form.Render (fun username password submitter ->
+        ......
+    )
+```
+Here we had to change two lines:
+1. Add an `onSubmit` parameter to our function, which has been inferred to a `string -> unit` function.
+2. Add a `Form.Run` call to our form, which in this case will be called on every submit, which in turn calls our `onSubmit` function.
+And now, all we have to do, besides implementing our shopping cart form, is routing, which can be done in a few lines, after specifying our endpoints.
+```fsharp
+open WebSharper
+open WebSharper.Sitelets
+open WebSharper.UI.Client
+
+type ClientEndpoint =
+| [<EndPoint "/">] Login
+| [<EndPoint "/">] ShoppingCart of userName:string
+
+let router = Router.Infer<ClientEndpoint>()
+let routerInstance = router.InstallHash Login router
+
+// the function that we can call either in a template hole or an HTML combinator
+let renderRoute() : Doc =
+    routerInstance.View
+    |> Doc.BindView (
+        function
+        | Login -> renderExampleForm (fun onSubmitParam -> ShoppingCart onSubmitParam |> routerInstance.Set )) // method can be simplified to (ShoppingCart >> routerInstance.Set)
+        | ShoppingCart userName -> renderShoppingCart userName // to be implemented
+```
--- FileInputExample/FileInputExample/FileInputExample/Client.fs
+++ FileInputExample/FileInputExample/FileInputExample/Client.fs
@@ -11,120 +11,184 @@ module Templates =
     type MainTemplate = Template<"index.html", ClientLoad.FromDocument, ServerLoad.WhenChanged>
 
 [<JavaScript>]
-module FormStuff =
-    open WebSharper.Forms
-    type Species =
-    | [<Constant "dog">] Dog
-    | [<Constant "cat">] Cat
-    with
-        override this.ToString() =
-            match this with
-            | Dog -> "dog"
-            | Cat -> "cat"
-    type Pet = {species:Species;name:string;alive:bool}
-                with static member Init () = {species=Dog;name="Fifi";alive=true}
-
-    type RegData = {
-        UserName:string
-        Password:string
-        
-    }
-
-
-    let RegForm() =
-        let init = {UserName="";Password=""}
-        Form.Return (fun uname pw -> {UserName=uname;Password=pw})
-        <*> (Form.Yield init.UserName
-            |> Validation.IsNotEmpty "Please provide a username!")
-        <*> (Form.Do {
-            let! pass = Form.Yield init.Password 
-            return! Form.Yield "" 
-                    |> Validation.IsNotEmpty "Please provide a password!"
-                    |> Validation.Is ((=) pass) "The two passwords must match!"
-        })
-        |> Form.WithSubmit
-
-    let RenderRegForm() =
-        RegForm()
-        |> Form.Render (fun username password submitter ->
-            Html.form [] [
-
-                Client.Doc.InputType.Text [] username
-                password.RenderPrimary(fun vi -> //Client.Doc.InputType.Password [] vi
-                    Doc.ShowErrors password.View (fun a -> Html.div [] [])
-                )
-                password.RenderDependent(fun a -> Client.Doc.InputType.Password [] a)
-                Doc.ShowErrors submitter.View (fun errMsgs -> 
-                    errMsgs 
-                    |> List.map (fun msg ->
-                        Html.span [Html.attr.color "red"] [Html.text $"{msg.Id}: {msg.Text}"]) 
-                    |> Doc.Concat 
-                )
-                Doc.ButtonValidate "Submit" [] submitter
-            ]
-            
-        )
-    
-    let PetForm () =
-        let init = Pet.Init()
-        Form.Return (fun s n isAlive -> { species = s; name = n; alive=isAlive })
-        <*> Form.Yield init.species
-        <*> (Form.Yield init.name
-            |> Validation.IsNotEmpty "Please enter the pet's name.")
-        <*> Form.Do {
-            
-            let! isAlive = Form.Yield true
-            return! Form.Yield isAlive
-        }
-        |> Form.WithSubmit
-
-    let PetListForm() =
-        Form.ManyForm Seq.empty (PetForm()) Form.Yield
-        |> Validation.Is(not << Seq.isEmpty) "Have at least one pet sadboi"
-        |> Form.WithSubmit
-        |> Form.Run (fun pets ->
-            ()
-        )
-
-    let valami() =
-        PetListForm()
-        |> Form.Render (fun pets submitter ->
-            Html.div [] [
-                pets.RenderAdder (fun a b dependent subm ->
-                    Doc.Concat [
-                        dependent.RenderPrimary (Client.Doc.InputType.CheckBox [])
-                        dependent.RenderDependent (Client.Doc.InputType.CheckBox [])
-                    ]
-                    
-                )
-            ]
-        )
-    let renderMainForm() =
-
-        PetListForm()
-        |> Form.Render (fun pets submitter ->
-            Html.div [] [
-                pets.RenderAdder (fun a b c d -> 
-                    Html.div [] [
-                        c.RenderPrimary (Client.Doc.InputType.CheckBox [])
-                        c.RenderDependent (fun (bVal:Var<bool>) ->
-                            bVal.View
-                            |> View.Map(function | true -> "success" | _ -> "fail")
-                            |> Html.textView
-                            
-                        )
-                    ])
-                
-            ]
-        )
-[<JavaScript>]
 module Client =
     open WebSharper.UI
+    open WebSharper.UI.Client
     open WebSharper.Forms
-    let Main () =
-        let tsk = task {
-            return "szoveg"
+
+    type AuthInfo = {
+        Username:string
+        Password:string
+    }
+        with 
+            static member Create username password = {Username=username;Password=password}
+            static member Default = AuthInfo.Create "" ""
+
+    type ClientEndpoint =
+    | [<EndPoint "/">] Home
+    | [<EndPoint "/login">] Login
+    | [<EndPoint "/sign-up"; Json "userData">] SignUp
+    | [<EndPoint "/";Json "userData">] Cart of userName:string
+    
+
+    module AuthForms =
+
+        let authFormBase() =
+            Form.Return (AuthInfo.Create)
+        let userNameBase() =
+            (Form.Yield AuthInfo.Default.Username
+                |> Validation.IsNotEmpty "Please provide a username")
+        let passwordBase() =
+            Form.Yield AuthInfo.Default.Password
+            // |> Validation.Is (insert someRegexCheck here) "errorMsg"
+            |> Validation.IsNotEmpty "Password cannot be empty!"
+        let passwordWithConfirm() =
+            Form.Do {
+                let! pass = passwordBase()
+                return! (Form.Yield AuthInfo.Default.Password
+                        |> Validation.Is ((=) pass) "The two passwords must match!")
+            }
+        let loginForm() =
+            authFormBase()
+            <*> userNameBase()
+            <*> passwordBase()
+            |> Form.WithSubmit
+
+        let ValamiForm() =
+            Form.Do {
+                let! user = userNameBase()
+                let! pass = passwordWithConfirm()
+                return {Username=user;Password=pass}
+            }
+            |> Form.WithSubmit
+        let signUpForm() =
+            authFormBase()
+            <*> userNameBase()
+            <*> passwordWithConfirm()
+            |> Form.WithSubmit
+        
+        let private renderErrors errors =
+                Doc.ShowErrors errors (fun errs -> 
+                    errs 
+                    |> List.map (fun err -> Html.div [] [Html.text err.Text]) 
+                    |> Doc.Concat)
+        let private renderPasswordDependent (passwordForm: Form.Dependent<string,Var<string> -> Doc,Var<string> -> Doc>) =
+            let inline renderFun view = Html.div [] [
+                Client.Doc.InputType.Password [] view
+                passwordForm.View |> renderErrors
+            ]
+            Doc.Concat [
+                passwordForm.RenderPrimary renderFun
+                passwordForm.RenderDependent renderFun
+            ]
+
+        let renderSubmit (submitter:Submitter<Result<_>>) =
+            Doc.Concat [
+                let showView = submitter.Input.Map(function | Success _ -> false | Failure _ -> true)
+                // Client.Doc.Button "Submit" [Html.attr.disabledBool showView] submitter.Trigger
+                Doc.ButtonValidate "Submit" [] submitter
+                submitter.View |> renderErrors
+
+            ]
+        let renderAuthForm (authForm:_ -> Form<AuthInfo,_>) passwordRender onSubmit =
+            authForm()
+            |> Form.Run (fun authSuccess -> onSubmit authSuccess.Username)
+            |> Form.Render (fun username password submitter ->
+                Html.form [] [
+                    Client.Doc.InputType.Text [] username
+                    passwordRender password
+                    renderSubmit submitter
+                ]
+            )
+        let renderLoginForm =
+            renderAuthForm loginForm (Client.Doc.InputType.Password [])
+        let renderSignUpForm =
+            renderAuthForm signUpForm renderPasswordDependent
+
+        
+    module ShoppingCart =
+        type CartItem = {
+            name:string
+            quantity:int
         }
+            with static member Init() = {name="";quantity=1}
+        type CheckoutFormData = {
+            name:string
+            address:string
+            email:string
+        }
+        let items = [
+            {name="Lepin Star Demolisher";quantity=1}
+            {name="Fight Robot CN";quantity=3}
+            {name="Protein bar";quantity=5}
+        ]
+
+        let itemsToOrder = items |> ListModel.Create (fun item -> item.name)
+        
+        let itemForm (init:CartItem) =
+            Form.Return (fun name quantity -> {name=name;quantity=quantity})
+            <*> (Form.Yield init.name
+                |> Validation.IsNotEmpty "Name cannot be empty")
+            <*> (Form.Yield init.quantity
+                |> Validation.Is (fun x -> x > 0) "Must order at least one of item!")
+        let cartForm it =
+            Form.Many it (CartItem.Init()) itemForm
+        let orderForm it =
+            Form.Return (fun name address email items -> {name=name;address=address;email=email})
+            <*> (Form.Yield ""
+                |> Validation.IsNotEmpty "Please enter a name")
+            <*> (Form.Yield ""
+                |> Validation.IsNotEmpty "Address pls")
+            <*> (Form.Yield ""
+                |> Validation.IsNotEmpty "Email pls")
+            <*> cartForm it
+            |> Form.WithSubmit
+        let renderCartForm it =
+            cartForm it
+            |> Form.Render (fun collection -> 
+                collection.Render (fun ops itemName quantity -> 
+                    Html.div [] [
+                        Html.div [] [Html.textView itemName.View]
+                        Html.div [] [Html.textView (quantity.View.Map(string))]
+                    ]
+                ))
+        let renderCartItems (collection: Form.Many.CollectionWithDefault<CartItem,(Var<string>->Var<int>->_),_>) =
+            collection.Render (fun ops itemName quantity -> 
+                    Html.div [] [
+                        Html.div [] [Html.textView itemName.View]
+                        Html.div [] [Html.textView (quantity.View.Map string)]
+                    ]
+                )
+        let renderOrdersForm onSubmit =
+            orderForm items
+            |> Form.Run (fun i -> onSubmit())
+            |> Form.Render (fun name address email coll submitter -> 
+                Html.div [] [
+                    Html.h2 [] [Html.text "Shopping cart"]
+                    renderCartItems coll
+
+                    Html.form [Html.attr.``class`` "d-flex flex-column"] [
+                        Html.label [Html.attr.``for`` "name"] [Html.text "Name"]
+                        Client.Doc.InputType.Text [Html.attr.name "name"] name
+                        Html.label [Html.attr.``for`` "address"] [Html.text "Address"]
+                        Client.Doc.InputType.Text [Html.attr.name "address"] address
+                        Html.label [Html.attr.``for`` "email"] [Html.text "Email"]
+                        Client.Doc.InputType.Email [Html.attr.name "email"] email
+                        Doc.ButtonValidate "Place order" [] submitter
+                    ]
+                ]
+                
+                
+                
+            )
+
+    open WebSharper.Sitelets
+    let Main () =
+
+        let router = Router.Infer<ClientEndpoint>()
+        let routerInstance = Router.InstallHash Login router
+
+        let asd = Form.Many [] "asd" Form.Yield
         let rvReversed = Var.Create "" 
         let datevar = 
             let date = Date(Date.Now())
@@ -132,10 +196,17 @@ module Client =
             |> Var.Create
         let dateInput = Client.Doc.InputType.Date [] datevar
         let colorvar = Var.Create("#0000ff")
+
         Templates.MainTemplate.MainForm()
             .DateVar(datevar)
             .ColorVar(colorvar)
-            .FormProbaSlot(FormStuff.RenderRegForm())
+            .FormProbaSlot(routerInstance.View 
+                |> Client.Doc.BindView (function
+                | Home | Login -> AuthForms.renderLoginForm (Cart >> routerInstance.Set)
+                | SignUp -> AuthForms.renderSignUpForm (Cart >> routerInstance.Set)
+                | Cart authInfo -> ShoppingCart.renderOrdersForm (fun () -> ())
+                )
+            )
             .OnFormSubmit(fun e ->
                 Console.Log "Submit clicked!"
                 let files = e.Vars.FilePicker.Value
