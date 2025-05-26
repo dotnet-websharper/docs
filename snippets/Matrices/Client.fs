fc2f6651e874ba009533c9e0a5805c6f9a60563f
Got <souvanxay38@gmail.com>
2025-05-26 14:26:27 +0200

    add snippet for Matrices

---
+++ snippets/Matrices/Client.fs
@@ -0,0 +1,58 @@
+namespace Matrices
+
+open WebSharper
+open WebSharper.JavaScript
+open WebSharper.UI
+open WebSharper.UI.Client
+open WebSharper.UI.Templating
+
+[<JavaScript>]
+module Client =
+    open WebSharper.MathJax
+    open WebSharper.MathJS
+
+    type IndexTemplate = Template<"wwwroot/index.html", ClientLoad.FromDocument>
+
+    [<SPAEntryPoint>]
+    let Main () =
+        Hub.Config(
+            MathJax.Config(
+                Extensions = [| "tex2jax.js" |],
+                Jax = [| "input/TeX"; "output/HTML-CSS"; |],
+                Tex2jax = Tex2jax(InlineMath = [| ("$", "$"); ("\\(", "\\)") |])
+            )
+        )
+
+        let matrix1 = Math.Matrix([| [| 8.; 6. |]; [| 4.; 6. |] |])
+        let matrix2 = Math.Matrix([| [| 6.; 14. |]; [| 8.; 2. |] |])
+
+        let toPrettyMatrix (matrix: float array array) =
+            "$" + ((Math.Parse(string matrix) |> As<Node>).ToTex()) + "$"
+
+        let prettyMatrix1 = toPrettyMatrix matrix1
+        let prettyMatrix2 = toPrettyMatrix matrix2
+
+        let result op = (op matrix1 matrix2)
+
+        let resultAdd = result (fun l r -> Math.Add(MathNumber(l), MathNumber(r)))
+        let resultSubtract = result (fun l r -> Math.Subtract(MathNumber(l), MathNumber(r)))
+        let resultMultiply = result (fun l r -> Math.Multiply(MathNumber(l), MathNumber(r)))
+        let resultDivide = result (fun l r -> Math.Divide(MathNumber(l), MathNumber(r)))
+
+        let toPrettyResult (resultMatrix: MathNumber) =
+            "$" + ((Math.Parse(string resultMatrix) |> As<Node>).ToTex()) + "$"
+
+        let prettyResultAdd = toPrettyResult resultAdd
+        let prettyResultSubtract = toPrettyResult resultSubtract
+        let prettyResultMultiply = toPrettyResult resultMultiply
+        let prettyResultDivide = toPrettyResult resultDivide
+
+        IndexTemplate.Main()
+            .Matrix1(prettyMatrix1)
+            .Matrix2(prettyMatrix2)
+            .ResultAdd(prettyResultAdd)
+            .ResultSubtract(prettyResultSubtract)
+            .ResultMultiply(prettyResultMultiply)
+            .ResultDivide(prettyResultDivide)
+            .Doc()
+        |> Doc.RunById "main"
---
+++ snippets/Matrices/vite.config.js
@@ -0,0 +1,10 @@
+﻿module.exports = {
+  root: "wwwroot",
+  build: {
+    rollupOptions: {
+      input: [
+        "./Scripts/Matrices.min.js"
+      ]
+    }
+  }
+}
