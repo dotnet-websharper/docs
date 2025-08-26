39fed89ed3fd46d6e118e338c3f772a04e4e9efe
Got <souvanxay38@gmail.com>
2025-08-26 16:11:31 +0200

    add DateFNS snippet code

---
+++ snippets/DateFNS/Client.fs
@@ -0,0 +1,49 @@
+namespace DateFNS
+
+open WebSharper
+open WebSharper.JavaScript
+open WebSharper.UI
+open WebSharper.UI.Client
+open WebSharper.UI.Templating
+open WebSharper.DateFNS
+
+[<JavaScript>]
+module Client =
+    // The templates are loaded from the DOM, so you just can edit index.html
+    // and refresh your browser, no need to recompile unless you add or remove holes.
+    type IndexTemplate = Template<"wwwroot/index.html", ClientLoad.FromDocument>
+
+    let private format = "yyyy-MM-dd"
+
+    [<SPAEntryPoint>]
+    let Main () =
+        let current = Var.Create (Date())
+        let nDays   = Var.Create 3
+
+        let viewTomorrow   = current.View |> View.Map (fun date -> DateFNS.AddDays(date, 1))
+        let viewStartMonth = current.View |> View.Map (fun d -> DateFNS.StartOfMonth(d))
+
+        let setNow () = current.Value <- Date()
+        let addDay () = current.Value <- DateFNS.AddDays(current.Value, 1)
+        let addWeek () = current.Value <- DateFNS.AddWeeks(current.Value, 1)
+        let toStartMo () = current.Value <- DateFNS.StartOfMonth(current.Value)
+        let addNDays () = current.Value <- DateFNS.AddDays(current.Value, nDays.Value)
+
+        IndexTemplate.Main()
+            .NowText(
+                current.View |> View.Map (fun date -> DateFNS.Format(date, format))
+            )
+            .TomorrowText(
+                viewTomorrow |> View.Map (fun date -> DateFNS.Format(date, format))
+            )
+            .StartOfMonthText(
+                viewStartMonth |> View.Map (fun date -> DateFNS.Format(date, format))
+            )
+            .NDays(nDays)
+            .SetNow(fun _ -> setNow())
+            .AddDay(fun _ -> addDay())
+            .AddWeek(fun _ -> addWeek())
+            .ToStartOfMonth(fun _ -> toStartMo())
+            .AddNDays(fun _ -> addNDays())
+            .Doc()
+        |> Doc.RunById "main"
---
+++ snippets/DateFNS/vite.config.js
@@ -0,0 +1,10 @@
+﻿module.exports = {
+  root: "wwwroot",
+  build: {
+    rollupOptions: {
+      input: [
+        "./Scripts/DateFNS.min.js"
+      ]
+    }
+  }
+}
