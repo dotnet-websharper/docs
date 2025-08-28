cb5da395750c58c1661ad05dc19f7660403e151e
András Jankó <andras.janko@gmail.com>
2025-08-28 11:07:05 +0200

    #32 dependent forms

---
+++ snippets/forms_example_2/Client.fs
@@ -0,0 +1,113 @@
+namespace forms_example_2
+
+open WebSharper
+open WebSharper.JavaScript
+open WebSharper.UI
+open WebSharper.UI.Client
+open WebSharper.UI.Templating
+open WebSharper.Forms
+open WebSharper.UI.Html
+
+// <FUMADOCS BEGIN>
+[<JavaScript>]
+module Client =
+
+    type Subject =
+        | Math | English | History
+        with override this.ToString() = $"%A{this}"
+
+    type TeacherOrStudent =
+        | Teacher of Subject 
+        | Student of year: int
+
+    let IsTeacherForm() = 
+        Form.Yield true
+
+    let TeacherForm() =
+        Form.Yield Math
+
+    let StudentForm() =
+        Form.Yield (Valid (1, "1")) // of type CheckedInput<int>
+        |> Validation.Is (function Valid _ -> true | _ -> false) "Please enter a valid number."
+        |> Validation.Is (function Valid (y, _) when y >= 1 && y <= 12 -> true | _ -> false) "Please enter a valid school year (1-12)."
+        |> Form.Map (function Valid (y, _) -> y | _ -> 0) //  by this time, we have already filtered out non-valid values
+
+    type TeacherOrStudentInput =
+        | TeacherInput of Var<Subject>
+        | StudentInput of Var<CheckedInput<int>>
+
+    let TeacherOrStudentForm() =
+        Form.Dependent (IsTeacherForm()) (fun isTeacher ->
+            if isTeacher then
+                TeacherForm()
+                |> Form.MapRenderArgs TeacherInput
+                |> Form.Map Teacher
+            else
+                StudentForm()
+                |> Form.MapRenderArgs StudentInput
+                |> Form.Map Student
+        )
+        |> Form.WithSubmit
+
+    let TeacherOrStudentForm2 =
+        Form.Do {
+            let! isTeacher = IsTeacherForm()
+            if isTeacher then
+                return!
+                    TeacherForm()
+                    |> Form.MapRenderArgs TeacherInput
+                    |> Form.Map Teacher
+            else
+                return!
+                    StudentForm()
+                    |> Form.MapRenderArgs StudentInput
+                    |> Form.Map Student
+        }
+        |> Form.WithSubmit
+
+    let ShowErrorMessage v =
+        v |> Doc.BindView (function
+            | Success _ -> Doc.Empty
+            | Failure msgs ->
+                div [attr.style "color:red"] [for msg in msgs -> p [] [text msg.Text] ]
+        )
+
+    let Form =
+        TeacherOrStudentForm2
+        |> Form.Render (fun dep submit ->            
+            div [] [
+                // render the primary IsTeacherForm
+                dep.RenderPrimary (fun rvIsTeacher ->
+                    p [] [
+                        label [] [Doc.InputType.Radio [] true rvIsTeacher; text "I am a teacher"]
+                        label [] [Doc.InputType.Radio [] false rvIsTeacher; text "I am a student"]
+                    ]
+                )
+                // render the dependent TeacherForm or StudentForm
+                dep.RenderDependent (fun rvTeacherOrStudent ->
+                    match rvTeacherOrStudent with
+                    | TeacherInput rvSubject ->
+                        Doc.InputType.Select [] string [Math; English; History] rvSubject
+                    | StudentInput rvYear ->
+                        Doc.Concat [
+                            Doc.InputType.Int [ attr.min "1"; attr.max "12" ] rvYear
+                            ShowErrorMessage (submit.View.Through rvYear)
+                        ]
+                )
+                p [] [Doc.Button "Submit" [] submit.Trigger]
+                submit.View |> View.Map (function
+                    | Success (Teacher subject) ->
+                        div [] [text ("You registered as a teacher of " + string subject + ".")]
+                    | Success (Student year) ->
+                        div [] [text ("You registered as a student in year " + string year + ".")]
+                    | _ -> Doc.Empty
+                )
+                |> Doc.EmbedView
+            ]
+        )
+
+    [<SPAEntryPoint>]
+    let Main () =
+        Form
+        |> Doc.RunById "main"
+// <FUMADOCS END>
---
+++ snippets/forms_example_2/vite.config.js
@@ -0,0 +1,10 @@
+﻿module.exports = {
+  root: "wwwroot",
+  build: {
+    rollupOptions: {
+      input: [
+        "./Scripts/forms_example_2.min.js"
+      ]
+    }
+  }
+}
