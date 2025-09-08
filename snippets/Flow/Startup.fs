e26659c6bffdc428fa261efb9c6e9941777be026
András Jankó <andras.janko@gmail.com>
2025-09-08 13:59:54 +0200

    fix Flow snippet

--- snippets/Flow/Startup.fs
+++ snippets/Flow/Startup.fs
@@ -4,7 +4,7 @@ open Microsoft.Extensions.Hosting
 open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.DependencyInjection
 open WebSharper.AspNetCore
-open Vars
+open Flow
 
 [<EntryPoint>]
 let main args =
